88b65c450d59e077db0d61e4a1c2f9ca2be9676d
Hank Tom <hanktom@gmail.com>
2022-03-07 15:17:51 +0800

    Toast and AlertDialog

--- app/src/main/java/com/tom/bmi2/MainActivity.kt
+++ app/src/main/java/com/tom/bmi2/MainActivity.kt
@@ -2,7 +2,10 @@ package com.tom.bmi2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.tom.bmi2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,12 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 
     fun bmi(view: View) {
-        println("hahaha")
+
         var weight = binding.edWeight.text.toString().toFloat()
         var height = binding.edHeight.text.toString().toFloat()
+        var bmi = weight/(height*height)
+        Log.d("MainActivity", bmi.toString())
+        Toast.makeText(this, "Your BMI $bmi", Toast.LENGTH_LONG).show()
+        /*val builder = AlertDialog.Builder(this)
+        builder.setTitle("Hello")
+        builder.setMessage("Your BMI is $bmi")
+        builder.setPositiveButton("OK", null)
+        val dialog = builder.create()
+        dialog.show()*/
+        AlertDialog.Builder(this)
+            .setTitle("Hello")
+            .setMessage("Your BMI is $bmi")
+            .setPositiveButton("OK", null)
+            .show()
     }
 }
